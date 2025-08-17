6d9afa479110deae77d05a013ed93bcc0662bf80
cowburrs <kazousatuma@proton.me>
2025-08-17 12:59:15 +1000

    feat(bash): new alias. cd.. = cd ..

--- bashrc/core/alias.sh
+++ bashrc/core/alias.sh
@@ -14,6 +14,7 @@ alias v='$EDITOR'
 alias grep='grep --color=auto'
 alias discord='discord --enable-features=UseOzonePlatform --ozone-platform=wayland'
 alias systemd-analyse='systemd-analyze'
+alias cd..='cd ..'
 
 # -----------------------------------------------------
 # Window Managers
